ab2945319ab7acc0e95297f8f73e004bcfcd47e3
rioto.nagano <kingdom950927@gmail.com>
2022-08-28 20:46:25 +0900

    multiply,divideメソッドの作成と結果の表示

--- 030_スコープ/920_修了確認_クロージャー/start/main.js
+++ 030_スコープ/920_修了確認_クロージャー/start/main.js
@@ -27,6 +27,16 @@ function calcFactory(val) {
       const newVal = val - target;
       console.log(val + " - " + target + " = " + newVal);
       val = newVal;
+    },
+    multiply: function(target) {
+      const newVal = val * target;
+      console.log(val + " * " + target + " = " + newVal);
+      val = newVal;
+    },
+    divide: function(target) {
+      const newVal = val / target;
+      console.log(val + " / " + target + " = " + newVal);
+      val = newVal;
     }
   };
 }
@@ -34,3 +44,5 @@ function calcFactory(val) {
 const calc = calcFactory(10);
 calc.plus(5);
 calc.minus(3);
+calc.multiply(3);
+calc.divide(2);
