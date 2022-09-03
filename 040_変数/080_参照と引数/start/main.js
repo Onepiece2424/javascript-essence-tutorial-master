f27954d4236574b64f849c9705b3a23accc62ae3
rioto.nagano <kingdom950927@gmail.com>
2022-09-03 18:32:19 +0900

    プリミティブ型とオブジェクトの値のコピー

--- 040_変数/080_参照と引数/start/main.js
+++ 040_変数/080_参照と引数/start/main.js
@@ -12,3 +12,20 @@ fn1(a);
 let arg1 = a;
 arg1 = 1;
 console.log(a, arg1);
+
+
+let b = {
+  prop: 0
+}
+
+function fn2(arg2) {
+  arg2.prop = 1;
+  console.log(b, arg2);
+}
+fn2(b);
+
+function fn3(arg2) {
+  arg2 = {};
+  console.log(b, arg2);
+}
+fn3(b);
