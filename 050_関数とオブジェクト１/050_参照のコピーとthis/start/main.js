cdf1abcbe5b6d8ae8e52f1415f66bb65e75af5b7
rioto.nagano <kingdom950927@gmail.com>
2022-09-06 06:06:42 +0900

    参照のコピーとthis

--- 050_関数とオブジェクト１/050_参照のコピーとthis/start/main.js
+++ 050_関数とオブジェクト１/050_参照のコピーとthis/start/main.js
@@ -4,21 +4,39 @@ const person = {
     name: 'Tom',
     hello: function() {
         console.log('Hello ' + this.name);
+        a();
+
+        const person = {
+            name: 'Tim',
+            hello: function() {
+                console.log('Hello ' + this.name);
+                a();
+            }
+        }
+        person.hello();
     }
 }
-const ref = person.hello;
-ref();
+// const ref = person.hello;
+// ref();
+
+person.hello();
+
+function a() {
+    console.log('Hello ' + this.name);
+}
 
 
-function doTwice(func) {
-    func(); // 1回目！
-    func(); // 2回目！
-  }
 
-  // あいさつするだけの関数
-  function hello() {
-    console.log('Hello!');
-  }
 
-  // あいさつを2回実行する
-  doTwice(hello);
+// function doTwice(func) {
+//     func(); // 1回目！
+//     func(); // 2回目！
+//   }
+
+//   // あいさつするだけの関数
+//   function hello() {
+//     console.log('Hello!');
+//   }
+
+//   // あいさつを2回実行する
+//   doTwice(hello);
